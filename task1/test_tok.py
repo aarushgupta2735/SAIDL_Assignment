3183bf56fa0f6690da217ec82a5736acd9a0a3fc
Aarush Gupta <aarushgupta2735@gmail.com>
2026-05-26 14:29:31 +0000

    Removed test_tok.py: not required

--- task1/test_tok.py
+++
@@ -1,9 +0,0 @@
-import time
-from bpetokenizer import BPETokenizer
-with open("data/wiki.train.txt") as f: data = f.read()
-
-tokenizer = BPETokenizer()
-start = time.time()
-print("Starting encode...")
-t = tokenizer.encode(data[:100000]) # just 100kb
-print(f"100kb Time: {time.time() - start:.2f}s")

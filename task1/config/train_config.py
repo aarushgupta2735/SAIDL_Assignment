baff831c8b9d291b0f45f2497a2a85b60ce29846
Aarush Gupta <aarushgupta2735@gmail.com>
2026-05-27 07:11:53 +0000

    Fixed configuration file for training

--- task1/config/train_config.py
+++ task1/config/train_config.py
@@ -4,10 +4,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 
-from config.transformer_config import TransformerConfig
-from config.train_config import TrainConfig
-
-
 @dataclass
 class TrainConfig():
     step_num: int = 1

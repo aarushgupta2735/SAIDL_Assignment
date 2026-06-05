b70795b0afa02e64a43f3c3b5a6cf404f4fef73a
Aarush Gupta <aarushgupta2735@gmail.com>
2026-06-05 10:57:40 +0000

    Fixed Replay Buffer get history and get batched_history

--- task2_TD3/src/ReplayBuffer.py
+++ task2_TD3/src/ReplayBuffer.py
@@ -1,23 +1,24 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from config.config import TD3config,TransformerConfig
+from config.config import TD3config, TransformerConfig
+
 
 class ReplayBuffer():
-    def __init__(self,config:TD3config,tConfig:TransformerConfig,device):
-        self.states = torch.zeros(config.D_size, config.obs_features,device=device)
-        self.actions = torch.zeros(config.D_size, config.act_features,device=device)
-        self.rewards = torch.zeros(config.D_size, 1,device=device)
-        self.next_states = torch.zeros(config.D_size, config.obs_features,device=device)
-        self.dones = torch.zeros(config.D_size, 1,device=device)
-        self.curr_D_size = 0 
-        self.n_envs = config.n_envs
-        self.D_size = config.D_size
-        self.L = tConfig.context_window
-
-    def sample(self,batch_size):
-        indices = torch.randint(0, min(self.curr_D_size,self.D_size), (batch_size,))
-        return (self.states[indices], self.actions[indices], self.rewards[indices], self.next_states[indices], self.dones[indices])
+    def __init__(self, config: TD3config, tConfig: TransformerConfig, device):
+        self.device = device
+        self.obs_features = config.obs_features
+        self.act_features = config.act_features
+        self.states      = torch.zeros(config.D_size, config.obs_features, device=device)
+        self.actions     = torch.zeros(config.D_size, config.act_features, device=device)
+        self.rewards     = torch.zeros(config.D_size, 1, device=device)
+        self.next_states = torch.zeros(config.D_size, config.obs_features, device=device)
+        self.dones       = torch.zeros(config.D_size, 1, device=device)
+        self.env_ids     = torch.zeros(config.D_size, dtype=torch.long, device=device)
+        self.curr_D_size = 0
+        self.n_envs      = config.n_envs
+        self.D_size      = config.D_size
+        self.L           = tConfig.context_window
 
     def add(self, state, action, reward, next_state, done):
         for i in range(self.n_envs):
@@ -27,24 +28,64 @@ class ReplayBuffer():
             self.rewards[idx]     = reward[i]
             self.next_states[idx] = next_state[i]
             self.dones[idx]       = done[i]
+            self.env_ids[idx]     = i
             self.curr_D_size += 1
 
-    def get_history(self,idx):
-        #returns last L pairs of states and actions for a given idx
-        history_states = []
+    def sample(self, batch_size):
+        indices = torch.randint(0, min(self.curr_D_size, self.D_size), (batch_size,))
+        return (
+            self.states[indices],
+            self.actions[indices],
+            self.rewards[indices],
+            self.next_states[indices],
+            self.dones[indices],
+            indices,
+        )
+
+    def get_history(self, idx):
+        env_id = self.env_ids[idx].item()
+        history_states  = []
         history_actions = []
-        padding_mask = torch.zeros((self.L,)).bool()
+
         for i in range(self.L):
-            curr_idx = (idx-i)%self.D_size
+            curr_idx = (idx - i) % self.D_size
+            if self.env_ids[curr_idx].item() != env_id:
+                break
             history_states.append(self.states[curr_idx])
             history_actions.append(self.actions[curr_idx])
-            padding_mask[i] = 1
-            if(self.dones[curr_idx==1]):
+            if i > 0 and self.dones[curr_idx].item() == 1:
                 break
-        #add zeros
-        history_actions+=history_actions+[0 for i in range(0,self.L-len(history_actions))]
-        history_states+=history_states+[0 for i in range(0,self.L-len(history_states))]
+
+        # Reverse so oldest is first
+        history_states  = history_states[::-1]
         history_actions = history_actions[::-1]
-        history_states = history_states[::-1]
-        ##NEED A PADDIGN MASK
-        return torch.tensor(history_states),torch.tensor(history_actions),padding_mask
+
+        valid_len = len(history_states)
+        pad_len   = self.L - valid_len
+
+        state_pad  = torch.zeros(pad_len, self.obs_features, device=self.device)
+        action_pad = torch.zeros(pad_len, self.act_features, device=self.device)
+
+        if valid_len > 0:
+            states_tensor  = torch.cat([state_pad,  torch.stack(history_states)],  dim=0)
+            actions_tensor = torch.cat([action_pad, torch.stack(history_actions)], dim=0)
+        else:
+            states_tensor  = state_pad
+            actions_tensor = action_pad
+
+        # True = masked (padding), False = valid
+        padding_mask = torch.ones(self.L, dtype=torch.bool, device=self.device)
+        padding_mask[pad_len:] = False
+
+        return states_tensor, actions_tensor, padding_mask
+
+    def batch_get_history(self, b_idx):
+        batch_states  = []
+        batch_actions = []
+        batch_masks   = []
+        for idx in b_idx:
+            s, a, m = self.get_history(idx.item())
+            batch_states.append(s)
+            batch_actions.append(a)
+            batch_masks.append(m)
+        return torch.stack(batch_states), torch.stack(batch_actions), torch.stack(batch_masks)
